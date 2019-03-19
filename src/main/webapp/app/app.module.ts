6e23d8139da2457bc28c24fbc0d46bc165d1a9c5
ming <minghsieh@datacom.com.tw>
2019-03-19 09:31:51 +0800

    Initial application generated by JHipster-5.8.2

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,66 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2Webstorage } from 'ngx-webstorage';
+import { NgJhipsterModule } from 'ng-jhipster';
+
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { JhipsterappSharedModule } from 'app/shared';
+import { JhipsterappCoreModule } from 'app/core';
+import { JhipsterappAppRoutingModule } from './app-routing.module';
+import { JhipsterappHomeModule } from './home/home.module';
+import { JhipsterappAccountModule } from './account/account.module';
+import { JhipsterappEntityModule } from './entities/entity.module';
+import * as moment from 'moment';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent, ActiveMenuDirective, ErrorComponent } from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-' }),
+        NgJhipsterModule.forRoot({
+            // set below to true to make alerts look like toast
+            alertAsToast: false,
+            alertTimeout: 5000,
+            i18nEnabled: true,
+            defaultI18nLang: 'en'
+        }),
+        JhipsterappSharedModule.forRoot(),
+        JhipsterappCoreModule,
+        JhipsterappHomeModule,
+        JhipsterappAccountModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+        JhipsterappEntityModule,
+        JhipsterappAppRoutingModule
+    ],
+    declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthExpiredInterceptor,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorHandlerInterceptor,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: NotificationInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [JhiMainComponent]
+})
+export class JhipsterappAppModule {
+    constructor(private dpConfig: NgbDatepickerConfig) {
+        this.dpConfig.minDate = { year: moment().year() - 100, month: 1, day: 1 };
+    }
+}
